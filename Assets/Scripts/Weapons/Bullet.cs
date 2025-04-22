c3ddf9b19ffc6292029189e77ac105952e3889c6
Your Name <you@example.com>
2025-04-22 13:08:23 -0300

    Added 2 layer of camera: The default one and other that draws object always on top of the scene

--- Assets/Scripts/Weapons/Bullet.cs
+++ Assets/Scripts/Weapons/Bullet.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 [RequireComponent(typeof(SphereCollider))]
 public class Bullet : MonoBehaviour
 {
-    private Rigidbody rb;
+    private int damage = 12;
+    private float createdTime = 0;
     [SerializeField] private float fireForce = 1000;
+
+    private Rigidbody rb;
+
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+
+        createdTime = Time.time;
     }
 
     public void Fire() => rb.AddForce(transform.forward * fireForce, ForceMode.Impulse);
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.TryGetComponent<Enemy>(out var enemy))
+            enemy.OnDamage(damage);
+
+        Destroy(gameObject);
+    }
 }
